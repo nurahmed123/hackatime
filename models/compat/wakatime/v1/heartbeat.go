3f5b23f467bc8ebe436a5e602b8f2bb09d982f94
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-09-28 10:40:29 -0400

    Revert "feat: track more fields for heatbeats"
    This reverts commit ad521bcd5ead7a5ce83ae08dbf978bb278cc0dbc.

--- models/compat/wakatime/v1/heartbeat.go
+++ models/compat/wakatime/v1/heartbeat.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/hackclub/hackatime/models"
-	"github.com/hackclub/hackatime/utils"
 )
 
 type HeartbeatsViewModel struct {
@@ -16,58 +15,39 @@ type HeartbeatsViewModel struct {
 // that is actually required for the import
 
 type HeartbeatEntry struct {
-	Id               string    `json:"id"`
-	Branch           string    `json:"branch"`
-	Category         string    `json:"category"`
-	Entity           string    `json:"entity"`
-	Editor           string    `json:"editor"`
-	IsWrite          bool      `json:"is_write"`
-	Language         string    `json:"language"`
-	Project          string    `json:"project"`
-	ProjectRootCount uint32    `json:"project_root_count"`
-	LineAdditions    uint32    `json:"line_additions"`
-	LineDeletions    uint32    `json:"line_deletions"`
-	Lines            uint32    `json:"lines"`
-	LineNumber       uint32    `json:"lineno"`
-	CursorPosition   uint32    `json:"cursorpos"`
-	Dependencies     string    `json:"dependencies"`
-	Time             float64   `json:"time"`
-	Type             string    `json:"type"`
-	UserId           string    `json:"user_id"`
-	MachineNameId    string    `json:"machine_name_id"`
-	OperatingSystem  string    `json:"operating_system"`
-	UserAgentId      string    `json:"user_agent_id"`
-	CreatedAt        time.Time `json:"created_at"`
+	Id            string    `json:"id"`
+	Branch        string    `json:"branch"`
+	Category      string    `json:"category"`
+	Entity        string    `json:"entity"`
+	IsWrite       bool      `json:"is_write"`
+	Language      string    `json:"language"`
+	Project       string    `json:"project"`
+	Time          float64   `json:"time"`
+	Type          string    `json:"type"`
+	UserId        string    `json:"user_id"`
+	MachineNameId string    `json:"machine_name_id"`
+	UserAgentId   string    `json:"user_agent_id"`
+	CreatedAt     time.Time `json:"created_at"`
 }
 
 func HeartbeatsToCompat(entries []*models.Heartbeat) []*HeartbeatEntry {
 	out := make([]*HeartbeatEntry, len(entries))
 	for i := 0; i < len(entries); i++ {
 		entry := entries[i]
-		opSys, editor, _ := utils.ParseUserAgent(entry.UserAgent)
 		out[i] = &HeartbeatEntry{
-			Id:               strconv.FormatUint(entry.ID, 10),
-			Branch:           entry.Branch,
-			Category:         entry.Category,
-			Entity:           entry.Entity,
-			Editor:           editor,
-			OperatingSystem:  opSys,
-			IsWrite:          entry.IsWrite,
-			Language:         entry.Language,
-			Project:          entry.Project,
-			ProjectRootCount: entry.ProjectRootCount,
-			LineAdditions:    entry.LineAdditions,
-			LineDeletions:    entry.LineDeletions,
-			Lines:            entry.Lines,
-			LineNumber:       entry.LineNumber,
-			CursorPosition:   entry.CursorPosition,
-			Dependencies:     entry.Dependencies,
-			Time:             float64(entry.Time.T().Unix()),
-			Type:             entry.Type,
-			UserId:           entry.UserID,
-			MachineNameId:    entry.Machine,
-			UserAgentId:      entry.UserAgent,
-			CreatedAt:        entry.CreatedAt.T(),
+			Id:            strconv.FormatUint(entry.ID, 10),
+			Branch:        entry.Branch,
+			Category:      entry.Category,
+			Entity:        entry.Entity,
+			IsWrite:       entry.IsWrite,
+			Language:      entry.Language,
+			Project:       entry.Project,
+			Time:          float64(entry.Time.T().Unix()),
+			Type:          entry.Type,
+			UserId:        entry.UserID,
+			MachineNameId: entry.Machine,
+			UserAgentId:   entry.UserAgent,
+			CreatedAt:     entry.CreatedAt.T(),
 		}
 	}
 	return out
