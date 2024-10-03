f83b445d7c1b54f73b10f200c460e4f56b177009
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-03 11:44:52 -0400

    chore: Update environment to "heroku" in config.yml and force SSL for Heroku deployment

--- middlewares/forceSSL.go
+++ middlewares/forceSSL.go
@@ -12,7 +12,7 @@ const (
 
 func ForceSsl(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if conf.Get().Env == "production" {
+		if conf.Get().Env == "heroku" {
 			if r.Header.Get(xForwardedProtoHeader) != "https" {
 				sslUrl := "https://" + r.Host + r.RequestURI
 				http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
