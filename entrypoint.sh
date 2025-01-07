50d7326e2fed98bd14fb1db37f39b861a6291664
Zach Latta <zach@zachlatta.com>
2025-01-07 15:35:31 -0500

    Revert "Make entrypoint match Procfile"
    This reverts commit 866daff03e1e86fddb54f1325178c19e2c2abe9b.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 if [ "$WAKAPI_DB_TYPE" == "sqlite3" ] || [ "$WAKAPI_DB_TYPE" == "" ]; then
   exec ./wakapi
 else
-  # echo "Waiting for database to come up"
-  # exec ./wait-for-it.sh   "$WAKAPI_DB_HOST:$WAKAPI_DB_PORT" -s -t 60 -- ./wakapi
-  ./wakapi -config config.yml
+  echo "Waiting for database to come up"
+  exec ./wait-for-it.sh   "$WAKAPI_DB_HOST:$WAKAPI_DB_PORT" -s -t 60 -- ./wakapi
 fi
